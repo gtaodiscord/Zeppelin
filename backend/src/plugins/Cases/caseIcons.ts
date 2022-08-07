2653fc3b18900e2bfcf067b1296fee66f900f47b
vcokltfre <vcokltfre@gmail.com>
2022-08-07 19:51:46 +0100

    chore: switch to GTAO case icons

--- backend/src/plugins/Cases/caseIcons.ts
+++ backend/src/plugins/Cases/caseIcons.ts
@@ -1,13 +1,13 @@
 import { CaseTypes } from "../../data/CaseTypes";
 
 export const caseIcons: Record<CaseTypes, string> = {
-  [CaseTypes.Ban]: "<:case_ban:742540201443721317>",
-  [CaseTypes.Unban]: "<:case_unban:742540201670475846>",
-  [CaseTypes.Note]: "<:case_note:742540201368485950>",
-  [CaseTypes.Warn]: "<:case_warn:742540201624338454>",
-  [CaseTypes.Kick]: "<:case_kick:742540201661825165>",
-  [CaseTypes.Mute]: "<:case_mute:742540201817145364>",
-  [CaseTypes.Unmute]: "<:case_unmute:742540201489858643>",
-  [CaseTypes.Deleted]: "<:case_deleted:742540201473343529>",
-  [CaseTypes.Softban]: "<:case_softban:742540201766813747>",
+  [CaseTypes.Ban]: "<:caseban:892052511698075689>",
+  [CaseTypes.Unban]: "<:caseunban:892052628345856080>",
+  [CaseTypes.Note]: "<:casenote:892052585765290014>",
+  [CaseTypes.Warn]: "<:casewarn:892052663154397225>",
+  [CaseTypes.Kick]: "<:casekick:892052550554107916>",
+  [CaseTypes.Mute]: "<:casemute:892052568308580392>",
+  [CaseTypes.Unmute]: "<:caseunmute:892052645911617606>",
+  [CaseTypes.Deleted]: "<:casedeleted:892052535332995072>",
+  [CaseTypes.Softban]: "<:casesoftban:892052606552264735>",
 };
